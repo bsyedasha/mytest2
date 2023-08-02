65afc9893463ecae212b37276bbac70e85a57f30
bsyedasha <140831578+bsyedasha@users.noreply.github.com>
2023-08-02 16:01:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,25 @@ terraform {
 provider "snowflake" {
 }
 
+resource "snowflake_warehouse" "warehouse" {
+  count = var.max_cluster_count == null ? 1 : 0
+
+  name           = var.name
+  comment        = var.comment
+  warehouse_size = var.size
+
+  initially_suspended   = var.initially_suspended
+  auto_resume           = var.auto_resume
+  auto_suspend          = var.auto_suspend
+  wait_for_provisioning = var.wait_for_provisioning
+
+  resource_monitor = var.resource_monitor
+
+  statement_timeout_in_seconds        = var.statement_timeout_in_seconds
+  statement_queued_timeout_in_seconds = var.statement_queued_timeout_in_seconds
+
+}
+
 resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo1"
